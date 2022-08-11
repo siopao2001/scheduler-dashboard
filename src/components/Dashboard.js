744b7c6760915d4d12e324cd09769abe54631e15
siopao2001 <sio_pao2001@yahoo.ca>
2022-08-11 12:40:36 -0400

    complete functions to set panel states

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,18 +4,63 @@ import classnames from "classnames";
 
 import Loading from "./Loading";
 
+import Panel from "./Panel";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
+
 class Dashboard extends Component {
   state = {
-    loading: false
+    loading: false,
+    focused: null
   }
+
+  selectPanel(id) {
+  
+  this.setState(previousState => ({
+   focused: previousState.focused !== null ? null : id
+  }));
+ }
+
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+       "dashboard--focused": this.state.focused
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    return <main className={dashboardClasses} />;
+    const panels = (this.state.focused ? data.filter(panel =>this.state.focused === panel.id) : data).map(panel => (
+      <Panel
+        key={panel.id}
+        label={panel.label}
+        value={panel.value}
+        onSelect={event => this.selectPanel(panel.id)}
+      />
+    ));
+
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
